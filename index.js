2e0fe413eaf395dd6fbffa90b70b5cf0c2b97ab3
Devin Mullin <devin.mullin90@gmail.com>
2021-09-05 11:01:15 -0500

    dom content loaded!

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The DOM has loaded');
+    let newText = document.getElementById('text')
+    newText.innerText = 'This is really cool!'
+})
+
+console.log('this is before DOM loads baby')
